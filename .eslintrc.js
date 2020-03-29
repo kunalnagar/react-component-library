91e66d7aefe58cad9b0057490f1eb06fa2b558d6
Kunal Nagar <knlnagar@gmail.com>
2020-03-29 15:49:57 -0400

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: ['eslint-config-kunalnagar'],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+}
---
+++ .storybook/main.js
@@ -0,0 +1,19 @@
+const path = require('path')
+
+module.exports = {
+  stories: ['../src/**/*.stories.tsx'],
+  addons: [
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+  ],
+  webpackFinal: async config => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      include: path.resolve(__dirname, '../src'),
+      use: [require.resolve('babel-loader')],
+    })
+    config.resolve.extensions.push('.ts', '.tsx')
+    return config
+  },
+}
