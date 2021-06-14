dc029630fc0ab7b6b1db72a05a35a2f071b6b47b
Kunal Nagar <knlnagar@gmail.com>
2021-06-14 00:12:37 -0400

    feat: New Component - TwitterTimelineEmbed (#126)

---
+++ src/components/TwitterTimelineEmbed/TwitterTimelineEmbed.tsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useState, useRef } from 'react'
+import { TwitterTimelineEmbedProps } from 'types'
+import { loadScript } from 'utils/loadScript'
+
+/**
+ * This component uses the Twitter Embedded timelines API: https://developer.twitter.com/en/docs/twitter-for-websites/timelines/guides/parameter-reference
+ */
+export const TwitterTimelineEmbed = ({
+  sourceType,
+  screenName,
+  slug,
+  showReplies = false,
+  chrome,
+  theme,
+  width,
+  height = 600,
+  tweetLimit,
+  borderColor,
+  ariaPolite = 'polite',
+  dnt = false,
+}: TwitterTimelineEmbedProps) => {
+  const [ready, setReady] = useState<boolean>(false)
+  const embedContainer = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    async function createTimeline() {
+      if (ready) {
+        const params = {
+          showReplies,
+          chrome,
+          theme,
+          width,
+          height,
+          tweetLimit,
+          borderColor,
+          ariaPolite,
+          dnt,
+          omit_script: true,
+        }
+        if (embedContainer.current) {
+          embedContainer.current.innerHTML = ''
+        }
+        await window.twttr.widgets.createTimeline(
+          {
+            sourceType,
+            screenName: sourceType === 'profile' ? screenName : undefined,
+            ownerScreenName: sourceType === 'list' ? screenName : undefined,
+            slug: sourceType === 'list' ? slug : undefined,
+          },
+          embedContainer.current,
+          params,
+        )
+      }
+    }
+    createTimeline()
+  }, [
+    ariaPolite,
+    borderColor,
+    chrome,
+    dnt,
+    height,
+    ready,
+    screenName,
+    showReplies,
+    slug,
+    sourceType,
+    theme,
+    tweetLimit,
+    width,
+  ])
+
+  useEffect(() => {
+    if (typeof window.twttr === 'undefined') {
+      loadScript(
+        {
+          url: 'https://platform.twitter.com/widgets.js',
+          id: 'twitter-wjs',
+          async: true,
+        },
+        () => {
+          setReady(true)
+        },
+      )
+    } else {
+      setReady(true)
+    }
+  }, [])
+
+  return <div ref={embedContainer} />
+}
---
+++ src/types/TwitterTimelineEmbed.ts
@@ -0,0 +1,74 @@
+interface BaseProps {
+  /**
+   * The screen name of the account
+   */
+  screenName: string
+  /**
+   * Show Tweets in response to another Tweet or account
+   *
+   * @default false
+   */
+  showReplies?: boolean
+  /**
+   * When set to none, only the cited Tweet will be displayed even if it is in
+   * reply to another Tweet.
+   */
+  chrome?: Array<
+    'noheader' | 'nofooter' | 'noborders' | 'transparent' | 'noscrollbar'
+  >
+  /**
+   * Display light text on a dark background
+   *
+   * @default light
+   */
+  theme?: 'dark'
+  /**
+   * Set the maximum width of the embedded Tweet. It is derived from the
+   * container size
+   *
+   * @default auto
+   */
+  width?: number
+  /**
+   * Set a fixed height of the embedded widget
+   *
+   * @default 600
+   */
+  height?: number
+  /**
+   * Render a timeline statically, displaying only n number of Tweets. The
+   * height parameter has no effect when a Tweet limit is set
+   *
+   * Accepted values: [1..20]
+   */
+  tweetLimit?: number
+  /**
+   * Adjust the color of borders inside the widget. Hexadecimal color.
+   */
+  borderColor?: string
+  /**
+   * Apply the specified aria-polite behavior to the rendered timeline. New
+   * Tweets may be added to the top of a timeline, affecting screen readers
+   *
+   * @default polite
+   */
+  ariaPolite?: 'polite' | 'assertive' | 'rude'
+  /**
+   * When set to true, the timeline and its embedded page on your site are not
+   * used for purposes that include personalized suggestions and personalized
+   * ads
+   */
+  dnt?: boolean
+}
+
+type VariableProps =
+  | {
+      sourceType: 'list'
+      slug: string
+    }
+  | {
+      sourceType: 'profile'
+      slug?: never
+    }
+
+export type TwitterTimelineEmbedProps = BaseProps & VariableProps
